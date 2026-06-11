1a334b12a02545634be5c7421cbe8bd7e80c2153
MerojT <merojbektursunboyev@gmail.com>
2026-06-11 13:46:34 +0500

    use DATABASE_URL

--- backend/src/config/data-source.js
+++ backend/src/config/data-source.js
@@ -8,13 +8,9 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
-  ssl: process.env.DB_HOST !== "localhost" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
   entities: [UserEntity, TodoEntity],
 });
